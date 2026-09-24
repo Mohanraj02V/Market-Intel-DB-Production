709e50534c7e55e342f43e836ee36cf06974b017
Mohan Raj <mohanrajvmvm221023@gmail.com>
2026-09-24 17:36:16 +0530

    production issue

--- backend/prospects/migrations/0022_remove_auditreverificationrequest_body_and_more.py
+++ backend/prospects/migrations/0022_remove_auditreverificationrequest_body_and_more.py
@@ -111,7 +111,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='outreachemail',
             name='tracking_token',
-            field=models.UUIDField(db_index=True, default=uuid.uuid4, editable=False, unique=True),
+            field=models.UUIDField(db_index=True, default=uuid.uuid4, editable=False),
         ),
         migrations.AddField(
             model_name='outreachemail',
